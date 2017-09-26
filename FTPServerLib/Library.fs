26956ea94b97a44125c82c4c55cfe93e257c39ed
VR <vrnithinkumar@gmail.com>
2017-09-26 19:26:35 +0530

    Saving initial ip & port stuff

--- FTPServerLib/Library.fs
+++ FTPServerLib/Library.fs
@@ -1,5 +1,18 @@
 namespace FTPServerLib
+open System
+open System.IO
+open System.Net
+open System.Net.Sockets
+open System.Threading
 
-module Say =
-    let hello name =
-        printfn "Hello %s" name
+module ServerHelpers =
+    let StartServer() =
+        let port = 11000
+        let ipHostInfo = Dns.Resolve(Dns.GetHostName())
+        let localIPAddress = ipHostInfo.AddressList.[0]
+        let localEndPoint = new IPEndPoint(localIPAddress, port)
+        printfn "ip Host : %A" ipHostInfo
+        printfn "ip IP Address: %A" localIPAddress
+        printfn "ip End Point: %A" localEndPoint
+
+open ServerHelpers
