2aa1783b7d5f9d0e3ba0c11a54d7d87b4cf9fbf7
vrnithinkumar <vrnithinkumar@gmail.com>
2017-09-26 23:32:42 +0530

    trying listern and debugging

--- FTPServerLib/Library.fs
+++ FTPServerLib/Library.fs
@@ -8,11 +8,15 @@ open System.Threading
 module ServerHelpers =
     let StartServer() =
         let port = 11000
-        let ipHostInfo = Dns.Resolve(Dns.GetHostName())
+        let ipHostInfo = Dns.GetHostEntry("localhost")
         let localIPAddress = ipHostInfo.AddressList.[0]
         let localEndPoint = new IPEndPoint(localIPAddress, port)
         printfn "ip Host : %A" ipHostInfo
         printfn "ip IP Address: %A" localIPAddress
         printfn "ip End Point: %A" localEndPoint
+        let s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp)
+        s.Bind(localEndPoint)
+        s.Listen(10)  
+        printfn "created socket"
 
-open ServerHelpers
+
--- FTPServerTest/Tests.fs
+++ FTPServerTest/Tests.fs
@@ -2,7 +2,8 @@ module Tests
 
 open System
 open Xunit
-
+open FTPServerLib.ServerHelpers
 [<Fact>]
 let ``My test`` () =
+    StartServer()
     Assert.True(true)
