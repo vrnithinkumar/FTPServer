95cea56d13ca429030edf566ca86f3c286930b81
vrnithinkumar <vrnithinkumar@gmail.com>
2017-09-29 20:50:50 +0530

    Unhandled Exception: System.Net.Sockets.SocketException: Address already in use

---
+++ ConsoleApp/Program.fs
@@ -0,0 +1,9 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open FTPServerLib.ServerHelpers
+[<EntryPoint>]
+let main argv =
+    printfn "Hello World from F#!"
+    StartServer()
+    0 // return an integer exit code
--- FTPServerLib/Library.fs
+++ FTPServerLib/Library.fs
@@ -6,17 +6,41 @@ open System.Net.Sockets
 open System.Threading
 
 module ServerHelpers =
-    let StartServer() =
-        let port = 11000
-        let ipHostInfo = Dns.GetHostEntry("localhost")
-        let localIPAddress = ipHostInfo.AddressList.[0]
-        let localEndPoint = new IPEndPoint(localIPAddress, port)
-        printfn "ip Host : %A" ipHostInfo
-        printfn "ip IP Address: %A" localIPAddress
-        printfn "ip End Point: %A" localEndPoint
+    type Socket with
+        member socket.AsyncAccept() = Async.FromBeginEnd(socket.BeginAccept, socket.EndAccept)
+    let CreateSocket port =
+        let localEndPoint = IPEndPoint(IPAddress.Parse("127.0.0.1"), port);  
         let s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp)
         s.Bind(localEndPoint)
-        s.Listen(10)  
+        s
+    let StartServer() =
+        let port = 11702
+        let s = CreateSocket port
+        s.Listen(111)  
+        let rec loop() = async {
+          printfn "Waiting for request ..."
+          let! socket = s.AsyncAccept()
+          // Setting ownsSocket to false allows us to later re-use a socket.
+          let stream = new NetworkStream(socket, false) 
+          printfn "Received request"
+          let response = [|
+            "HTTP/1.1 200 OK\r\n"B
+            "Content-Type: text/plain\r\n"B
+            "\r\n"B
+            "Hello World!"B |] |> Array.concat
+          try
+            try
+              let! bytesSent = stream.AsyncWrite(response)
+              printfn "Sent response"
+            with e -> printfn "An error occurred: %s" e.Message
+          finally
+            stream.Close()
+            socket.Shutdown(SocketShutdown.Both)
+            socket.Close()
+          return! loop() 
+          }
+
+        Async.Start(loop())
         printfn "created socket"
 
-
+    StartServer()
