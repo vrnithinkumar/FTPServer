e438fea600403dacd6884ef82d1bdce821f073c6
vrnithinkumar <vrnithinkumar@gmail.com>
2017-11-03 23:58:31 +0530

    Checking in fsi script

--- InteractiveTesting.fsx
+++ InteractiveTesting.fsx
@@ -1,3 +1,5 @@
-#I __SOURCE_DIRECTORY__
-#r @"./ConsoleApp/bin/Debug/netcoreapp2.0/ConsoleApp.dll"
-#r @"/Users/vr/WorkSpace/GitHub/FTPServer/FTPServerLib/bin/Debug/netcoreapp2.0/FTPServerLib.dll"
+#r @"/Users/vr/WorkSpace/GitHub/FTPServer/FTPServerLib/bin/Debug/netstandard2.0/FTPServerLib.dll"
+open FTPServerLib.Main
+open FTPServerLib.ClientHelpers
+StartServer()
+CreateClient()
